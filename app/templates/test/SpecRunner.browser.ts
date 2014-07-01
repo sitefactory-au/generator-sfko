68d98558fa2fdcad176283ba74644d5b6e72d690
David Sichau <git@sichau.eu>
2014-07-01 14:16:32 +0200

    the generator was updated to the newest dependencies and the component also adds an test

--- app/templates/test/SpecRunner.browser.ts
+++ app/templates/test/SpecRunner.browser.ts
@@ -1,7 +1,9 @@
 module main {
     // Reference your test modules here
     var testModules = [
-        'components/home-page'
+        // [Scaffolded component registrations will be inserted here. To retain this feature, don't remove this comment.]
+
+        'components/home-page/home-page'
     ];
 
     // After the 'jasmine-boot' module creates the Jasmine environment, load all test modules then run them
