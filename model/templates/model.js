0e2bca1a4755de8d56456f6c515e4d413b15d405
Luke Gardiner <luke@sitefactory.com.au>
2014-07-11 10:42:43 +1000

    model.js template now uses this rather than self Fixed folder includes in package.json

--- model/templates/model.js
+++ model/templates/model.js
@@ -1,15 +1,14 @@
 define(['knockout'], function(ko) {
 
   function <%= modelClassName %>(data) {
-    var self = this;
-    self.id = data.id;
-    self.name = ko.observable(data.name);
+    this.id = this.id;
+    this.name = ko.observable(data.name);
   }
 
   // This runs when the component is torn down. Put here any logic necessary to clean up,
   // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
   <%= modelClassName %>.prototype.dispose = function() { };
-  
+
 
   return <%= modelClassName %>;
 
